10b3e8e9fdc4e8cfd112203851693bad492022ee
MFors <fors.mikael@gmail.com>
2014-07-15 13:33:27 +0200

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,10 @@
+'''
+ProjectEuler Problem 13
+
+Mikael Fors 2014
+
+"Work out the first ten digits of the sum of the following one-hundred 50-digit
+ numbers."
+'''
+with open('13.data', 'r') as raw:
+    print str(sum(map(int, raw.read().split('\n'))))[:10]
