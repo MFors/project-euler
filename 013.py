21fe09d93846facb1c5ce3125f7e690f71877ec4
MFors <fors.mikael@gmail.com>
2014-07-15 13:51:39 +0200

    Update 013.py

--- 013.py
+++ 013.py
@@ -6,5 +6,5 @@ Mikael Fors 2014
 "Work out the first ten digits of the sum of the following one-hundred 50-digit
  numbers."
 '''
-with open('13.data', 'r') as raw:
+with open('013.data', 'r') as raw:
     print str(sum(map(int, raw.read().split('\n'))))[:10]
