81dcac1e6e1ece6cacf63db91706aed6a5320de4
Mikael Fors <fors.mikael@gmail.com>
2014-07-28 19:17:36 +0200

    Update 001.py

--- 001.py
+++ 001.py
@@ -5,10 +5,7 @@ Mikael Fors 2014
 
 "Find the sum of all the multiples of 3 or 5 below 1000."
 '''
-print sum((n for n in xrange(1, 1000) if not n % 3 or not n % 5))
-
-
-# An even more efficient way is to note that there are 999//5 multiples of 5
+# An efficient way is to note that there are 999//5 multiples of 5
 # and this means that there are in total t_{999//5} counts, resulting in
 # a sum of (999//5 ** 2 + 999//5) >> 1
 
